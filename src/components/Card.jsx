712597157980d0e1654840f90cf2fb7c71c8f404
Carlos Sánchez <holapueblodev@gmail.com>
2023-01-26 14:12:57 +0100

    service Sakura

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,38 @@
-import api from api
+import "../App.css";
+import { useState, useEffect } from "react";
+import { sakuraService } from "./sakuraService";
 
+function App() {
+  const [dataRandom, toPrintCard] = useState([]);
+  
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>Hello, world!</h1>);
+useEffect(() =>{
+    sakuraService().then(data => {
+    const dataRandom = data.sort(function () {return Math.random() - 0.5;})
+    toPrintCard(dataRandom)})
+}, []);
+    
+  return (
+    <div class="grid-container">
+      <div class="empty-space"></div>
+      <div class="cards-grid">
+        {dataRandom.map((element) => {
+          return (
+            <>
+              <div key={element.id} class="flip-card">
+                <div class="flip-card-inner">
+                  <div class="flip-card-front">
+                    <img src={element.cardsReverse.sakuraReverse} alt=""></img>
+                  </div>
+                  {/* <div class="flip-card-back" style={{ backgroundImage: `url(${element.sakuraCard})` }}></div> */}
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/sakuraService.jsx
@@ -0,0 +1,17 @@
+
+
+export function sakuraService() {
+  const url = "https://6388b6e5a4bb27a7f78f96a5.mockapi.io/sakura-cards/";
+  try {
+    const response = fetch(
+      url
+    )
+      .then((response) => response.json())
+      .then((data) => data);
+    return  response;
+  } catch (error) {
+    return [];
+  }
+}
+
+
