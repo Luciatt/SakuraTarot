f9ce07d2aab42ac2da05daab60fb8e76b2d074bb
lucia <luciatiradoturiel@gmail.com>
2023-02-02 15:45:35 +0100

    finish

--- src/App.js
+++ src/App.js
@@ -6,45 +6,58 @@ import Prediction from './components/Prediction';
 
 export let selectCards = []
 
+let paragraph = "Escoge una carta para interpretar tu pasado";
 function App() {
 
   const [pastOn, setPast] = useState(false);
   const [presOn, setPres] = useState(false);
   const [futOn, setFut] = useState(false);
   const [rotate, setRotate] = useState(false);
-
-  const handleClick = (item) => {
-    if (selectCards.length < 3){
-        selectCards.push(item)
-        console.info(selectCards);
-        console.info(pastOn)
-        console.info(selectCards.length)
-        setPast(true)}
-    if(selectCards.length >= 2) { setPres(true) } 
-    if(selectCards.length >= 3) { setFut(true) }
-  }
+  const [textOff, setTextOff] = useState(true);
+  const [text, setText] = useState(true);
   
+  const handleClick = (item) => {
+
+    if (selectCards.length < 3 && text === false) {
+      selectCards.push(item)
+      setPast(true)
+    }
+    if (selectCards.length >= 1) {
+      paragraph = "De acuerdo. Ya tenemos tu carta vinculada al pasado. Ahora, escoge una para analizar tu presente... "
+    }
+    if (selectCards.length >= 2) {
+      paragraph = "Por último, elige una tercera carta para ver tu futuro..."
+      setPres(true)
+    }
+    if (selectCards.length >= 3) {
+      setTextOff(false)
+      setFut(true)
+    }
+
+  }
+
   const rotateCards = () => {
     setRotate(true)
   }
-  
+
   const past = selectCards[0];
   const present = selectCards[1];
   const future = selectCards[2];
-  
+
+
 
   return (
     <>
-      <AllCards handleClick={handleClick}/>
+      <AllCards text={text} handleClick={handleClick} />
       <div className='select-container'>
-      <div className="select-cards-grid">
-        <UnselectedCard rotate={rotate} pastOn={pastOn} selectedCard={past} text='PASADO' />
-        <UnselectedCard rotate={rotate} pastOn={presOn} selectedCard={present} text='PRESENTE'/>
-        <UnselectedCard rotate={rotate} pastOn={futOn} selectedCard={future} text='FUTURO' />
+        <div className="select-cards-grid">
+          <UnselectedCard rotate={rotate} pastOn={pastOn} selectedCard={past} text='PASADO' />
+          <UnselectedCard rotate={rotate} pastOn={presOn} selectedCard={present} text='PRESENTE' />
+          <UnselectedCard rotate={rotate} pastOn={futOn} selectedCard={future} text='FUTURO' />
+        </div>
       </div>
-      </div>
-      <Prediction rotateCards={rotateCards}  past={past} present={present} future={future} />
-      
+      <Prediction text={text} setText={setText} textOff={textOff} paragraph={paragraph} rotateCards={rotateCards} past={past} present={present} future={future} />
+
     </>
   );
 }
--- src/components/Prediction.jsx
+++ src/components/Prediction.jsx
@@ -1,32 +1,48 @@
 import React, { useState }from 'react'
 import { selectCards } from '../App';
 import button from "../img/BotonPrediccion.svg";
+import Dialogue from './Dialogue';
 
-function Prediction({ past, present, future, rotateCards }) {
+
+
+function Prediction(props) {
     const [buttOn, setButton] = useState(true);
+    
 
     const seePrediction = () => {
         if (selectCards.length === 3) {
             setButton(false);
-            rotateCards();
+            props.rotateCards();
         }
     }
+    console.info(props.textOff)
     return (
 
         <>
             {
                 buttOn ?
                     <>
+                    {
+                        props.textOff ?
+                        <>
+                        <Dialogue setText={props.setText} text ={props.text}paragraph={props.paragraph}/>
+                        </>
+                        :
+                        <>
                         <div className='prediction'>
-                            <button onClick={() => seePrediction()} className="buttonPrediction"><img  src={button}></img></button>
+                            <button onClick={() => seePrediction()} className="buttonPrediction"><img  src={button} alt =""></img></button>
                         </div>
+                        </>
+                    }
+
+                        
                     </>
                     :
                     <>
                         <div className='prediction'>
-                            <h3> Pasado: {past.meaning}</h3>
-                            <h3> Presente: {present.meaning}</h3>
-                            <h3> Futuro: {future.meaning}</h3>
+                            <h3> Pasado: <span>{props.past.englishName}</span> - <i>"{props.past.meaning}"</i></h3>
+                            <h3> Presente: <span>{props.present.englishName}</span> - <i>"{props.present.meaning}"</i></h3>
+                            <h3> Futuro: <span>{props.future.englishName}</span> - <i>"{props.future.meaning}"</i></h3>
                         </div>
                     </>
             }
